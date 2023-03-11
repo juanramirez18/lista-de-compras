d2ca2b04f1477d63c258f7fecabfa91236bb44ad
juanramirez18 <juanramirez189@hotmail.com>
2023-03-11 17:15:57 -0500

    6 commit

--- js/app.js
+++ js/app.js
@@ -1,11 +1,27 @@
 const botonCoche = document.querySelector("#hamburguesas");
 botonCoche.onclick = (e) =>{
-    valorProducto(e.target.value);
+    valorProducto(e.target.id);
 
 };
 
 function valorProducto(value){
-    console.log(value)
 
+    if(value == "op1"){
+        mostrarHTML("Hamburguesa 300 GR")
+    }else if(value == "op2"){
+        mostrarHTML("Hamburguesa mixta")
+    }else{
+        mostrarHTML("Hamburguesa crispy")
+    }
+
+}
+
+function mostrarHTML(mensaje){
+    const resultado = document.querySelector("#resultado");
+    const parrafo = document.createElement("P");
+    parrafo.classList.add("fs-3", "fw-bold", "mt-5")
+    parrafo.textContent = mensaje;
+    console.log(parrafo)
+    resultado.appendChild(parrafo);
 }
 
