f45b26340e582d6b277459bfe290556a6b50b35f
juanramirez18 <juanramirez189@hotmail.com>
2023-03-13 10:06:49 -0500

    10 commit

--- js/app.js
+++ js/app.js
@@ -1,29 +1,46 @@
-const botonCoche = document.querySelector("#hamburguesas");
-botonCoche.addEventListener("click", (e) => valorProducto(e.target.id, precios))
+const botonHamburguesa = document.querySelector("#hamburguesas");
+const bebidas = document.querySelector("#bebidas")
+botonHamburguesa.addEventListener("click", (e) => valorProducto(e.target.id, precios))
 // botonCoche.onclick = (e) =>{
 //     valorProducto(e.target.id, precios);
 
 // };
+
+bebidas.onclick = (e) => {
+    valorProducto(e.target.id, precios)
+}
 let total = [];
 const precios = {
-    precio1 : 2300,
-    precio2 : 1300,
-    precio3: 1200
+    precio1 : 21000,
+    precio2 : 23000,
+    precio3: 25000, 
+    precio4: 8000,
+    precio5: 3000,
+    precio6: 3500
 }
 
 function valorProducto(value, precios){
-    const {precio1, precio2, precio3} = precios;
+    const {precio1, precio2, precio3, precio4, precio5, precio6} = precios;
 
     if(value == "op1"){
-        mostrarHTML("Hamburguesa 300 gr $" ,  precio1)
+        mostrarHTML("Hamburguesa 300gr $" ,  precio1)
         total.push(precio1)
         
     }else if(value == "op2"){
-        mostrarHTML("Hamburguesa mixta ", precio2)
+        mostrarHTML("Hamburguesa mixta $", precio2)
         total.push(precio2)
-    }else{
-        mostrarHTML("Hamburguesa crispy $23.5k", precio3)
+    }else if(value == "op3"){
+        mostrarHTML("Hamburguesa crispy $", precio3)
         total.push(precio3)
+    }else if(value == "op4"){
+        mostrarHTML("Cerveza $", precio4)
+        total.push(precio4)
+    }else if(value == "op5"){
+        mostrarHTML("Gaseosa $", precio5)
+        total.push(precio5)
+    }else if(value == "op6"){
+        mostrarHTML("Botella de agua $", precio6)
+        total.push(precio6)
     }
 
     mostrarTotal(total);
@@ -33,11 +50,15 @@ function valorProducto(value, precios){
 }
 
 function mostrarHTML(mensaje, precio){
-    const resultado = document.querySelector("#resultado");
-    const parrafo = document.createElement("P");
-    parrafo.classList.add("fs-5")
-    parrafo.textContent = `${mensaje} ${precio}`;
-    resultado.appendChild(parrafo);
+    
+        const resultado = document.querySelector("#resultado");
+        const parrafo = document.createElement("P");
+        parrafo.classList.add("fs-5")
+        parrafo.textContent = `${mensaje} ${precio}`;
+        resultado.appendChild(parrafo);
+
+    
+   
 };
 
 function mostrarTotal(total){
